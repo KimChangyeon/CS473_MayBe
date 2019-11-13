6e6bc63f1cbd56c3fe725fc7603b8027f60ca006
KimChangyeon <doli1573@kaist.ac.kr>
2019-11-13 21:56:51 +0900

    Basic Layout
    기본적인 페이지 구성 완료

--- maybe/src/App.js
+++ maybe/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {DropdownButton, Dropdown} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+
+  state = {
+    header: <header>
+              <div className = "wrapper">
+                <h1>MayBe</h1>
+                <nav>
+                  <ul className="menu">
+                    <li><a href="#">Timetable</a></li>
+                    <li><a href="#">Relationship</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </header>,
+    page_id: 0,
+    page : ['home','scenario','about','feedback'],
+    stage_id: 0,
+    stage : ['intro','discription'],
+    language_list : ['English', 'Korean'],
+    language: 0,
+    stopper: 0,
+    request: 0
+  }
+
+  render () {
+    return (
+    <div>
+      <header>
+        {this.state.header}
+        <body>
+          <div className='wrap'>
+            main page sample.
+          </div>
+        </body>
+        
       </header>
     </div>
-  );
+    )
+  };
 }
 
 export default App;
