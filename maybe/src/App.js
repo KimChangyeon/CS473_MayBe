e145440d5b0bd550414f04fa7a4cd90ef8d0a9b1
상호 <users@userui-MacBook-Pro-2.local>
2019-11-14 22:29:19 +0900

    css setting

--- maybe/src/App.js
+++ maybe/src/App.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-import {DropdownButton, Dropdown} from 'react-bootstrap'
+import {DropdownButton, Dropdown} from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 
 class App extends Component {
 
   state = {
     header: <header>
               <div className = "wrapper">
-                <h1>MayBe</h1>
+                <h1 className="Darkblue"><a href="#">MayBe</a></h1>
                 <nav>
                   <ul className="menu">
-                    <li><a href="#">Timetable</a></li>
-                    <li><a href="#">Relationship</a></li>
+                    <li><a href="#"><Image src={require('./icon/Show_Menu.png')}/></a></li>
                   </ul>
                 </nav>
               </div>
@@ -32,15 +32,30 @@ class App extends Component {
       <header>
         {this.state.header}
         <body>
+					<Bar title="bar"/>
           <div className='wrap'>
             main page sample.
           </div>
-        </body>
-        
+        </body>  
       </header>
     </div>
     )
-  };
+  }
+}
+
+class Bar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: this.props.title,
+		};
+	}
+
+	render () {
+		return (
+			<div className="square" color="red">{this.state.title}</div>
+		)
+	}
 }
 
 export default App;
