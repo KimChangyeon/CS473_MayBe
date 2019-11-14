c821e34ae43cd9732dd0bbc8a6ed22b9a82dd266
Lim-Sangho <lim.sang@kaist.ac.kr>
2019-11-15 00:50:07 +0900

    structure

--- maybe/src/App.js
+++ maybe/src/App.js
@@ -1,21 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import {DropdownButton, Dropdown} from 'react-bootstrap';
-import Image from 'react-bootstrap/Image';
+import Upcoming from './Upcoming.js';
 
 class App extends Component {
-
   state = {
-    header: <header>
-              <div className = "wrapper">
-                <h1 className="Darkblue"><a href="#">MayBe</a></h1>
-                <nav>
-                  <ul className="menu">
-                    <li><a href="#"><Image src={require('./icon/Show_Menu.png')}/></a></li>
-                  </ul>
-                </nav>
-              </div>
-            </header>,
     page_id: 0,
     page : ['home','scenario','about','feedback'],
     stage_id: 0,
@@ -28,34 +16,9 @@ class App extends Component {
 
   render () {
     return (
-    <div>
-      <header>
-        {this.state.header}
-        <body>
-					<Bar title="bar"/>
-          <div className='wrap'>
-            main page sample.
-          </div>
-        </body>  
-      </header>
-    </div>
+			<Upcoming/>
     )
   }
-}
-
-class Bar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: this.props.title,
-		};
-	}
-
-	render () {
-		return (
-			<div className="square" color="red">{this.state.title}</div>
-		)
-	}
 }
 
 export default App;
