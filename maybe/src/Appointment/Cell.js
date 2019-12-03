9dd28dd86339147bb3bf14b731e1d494157716f8
wltn0029 <wltn0029@kaist.ac.kr>
2019-12-04 04:40:44 +0900

    fix minor problem

--- maybe/src/Appointment/Cell.js
+++ maybe/src/Appointment/Cell.js
@@ -55,18 +55,28 @@ class Cell extends Component {
                 }
             case "Vote" :
                 console.log ("props type : vote")
-                if (selected) {
-                    if (selectNum >= 5) selectNum = 5
-                    else selectNum += 1
+                if (selected) selectNum +=1
+                if (selectNum > 5) selectNum = 5
+                if (selectNum >= 1) {
+                    return(
+                        <td
+                            onClick = {this.handleClick}
+                            style = {{backgroundColor : bgColors[selectNum]}}
+                        >
+                            {this.renderHeart()}
+                        </td>
+                    )
                 }
-                return(
-                    <td
+                
+                else {
+
+                    return (
+                        <td
                         onClick = {this.handleClick}
-                        style = {{backgroundColor : bgColors[selectNum]}}
-                    >
-                        {this.renderHeart()}
-                    </td>
-                )
+                        >
+                        </td>
+                    )
+                }
              }
     }
 }
